d68178c02721ad8a9ee9574b148f7cf03c528170
j3n4r3v <jene4ka4reva@gmail.com>
2020-10-13 01:09:02 +0300

    ele-ele blin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const pictureTemplate = document.querySelector(`#picture`) // Контент шаблона откуда копируем данные
+  .content
+  .querySelector(`.picture`);
+const pictureContainer = document.querySelector(`.pictures`); // Куда вставляем скопированные данные
+let pictureLikes = pictureTemplate.querySelector(`.picture__likes`);
+let pictureComments = pictureTemplate.querySelector(`.picture__comments`);
+let pictureImg = pictureTemplate.querySelector(`.picture__img`);
+
 const COMMENT_MESSAGES = [
   {comment: `Всё отлично!`},
   {comment: `В целом всё неплохо. Но не всё.`},
@@ -19,17 +27,18 @@ const COMMENT_NAMES = [
 ];
 
 let OBJECT_COUNT = {
-  url: 'photos/i.jpg',
-  description: 'Описание фотографии',
-  likes: 'getRandomLikes(15, 200)',
+  url: `photos/i.jpg`,
+  description: `Описание фотографии`,
+  likes: `getRandomLikes(15, 200)`,
   comments: {
-    avatar: 'img/avatar-getRandomAc(1, 6).svg',
-    message: 'getRandomAc(1, 6)',
-    name: 'getRandomAc(1, 6)'
+    avatar: `img/avatar-getRandomAc(1, 6).svg`,//Пробовал конкатенацией соединить - не получается :(
+    message: `COMMENT_MESSAGES[getRandomAc(1, 6)]`,
+    name: `COMMENT_NAMES[getRandomAc(1, 6)]`
   }
 };
 
-let MASSIVE_OBJECT_JS = [];
+let MASSIVE_OBJECT_JS = [OBJECT_COUNT[i]]; //Массив куда будем добавлять 25 сгенерированных Js обьектов
+
 //Функциu генерации случайных данных
 
 function getRandomAc(min, max) { // Рандомные значения avatar-message-name
@@ -40,12 +49,22 @@ function getRandomLikes(min, max) { // Рандомные значения likes
   return Math.random() * (max - min) + min;
 };
 
-// Функция заполнения блока DOM-элементами на основе массива JS-объектов
-
-for (i = 0; i < 25; i++) { // Цикл перебора photos от 1-25
-};
 //Функция создания DOM-элемента на основе JS-объекта
-let ARRAY_CREATE = function (OBJECT_COUNT[i]) {
-  MASSIVE_OBJECT_JS[i] = MASSIVE_OBJECT_JS.push(OBJECT_COUNT[i]);
-  return MASSIVE_OBJECT_JS;
+let RENDER_OBJECT_COUNT = function (MASSIVE_OBJECT_JS) {
+  for (let i = 0; i < MASSIVE_OBJECT_JS.length; i++) { // Цикл перебора photos от 1-25
+    let pictureElement = pictureTemplate.cloneNode(true);
+    pictureContainer.appendChild(pictureElement);
+    pictureLikes.textContent = getRandomLikes(15, 200);
+    OBJECT_COUNT.url = pictureImg.src.textContent;
+  };
+  return pictureElement;
 };
+
+// Функция заполнения блока DOM-элементами на основе массива JS-объектов
+let fragment = document.createDocumentFragment();
+for (var i = 0; i < MASSIVE_OBJECT_JS.length; i++) {
+  fragment.appendChild(RENDER_OBJECT_COUNT(MASSIVE_OBJECT_JS[i]));
+}
+pictureContainer.appendChild(fragment);
+
+/*Я старался понять и сделать сам - но увы,не доходит до конца :(*/
