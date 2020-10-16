ba68308908782ad18f45685c096f508fecda4d36
j3n4r3v <jene4ka4reva@gmail.com>
2020-10-16 17:44:51 +0300

    done

--- js/main.js
+++ js/main.js
@@ -84,3 +84,53 @@ let renderPhoto = function (photo) {
   });
   PICTURE_CONTAINER.appendChild(photosFragment);
 }());
+
+// 3.2
+
+const renderBigPicture = (photo) => {
+  const bigPicture = document.querySelector(`.big-picture`);
+  bigPicture.classList.remove(`hidden`);
+
+  bigPicture.querySelector(`.big-picture__img img`).setAttribute(`src`, `photo.url`);
+  bigPicture.querySelector(`.likes-count`).textContent = photo.likes;
+  bigPicture.querySelector(`.comments-count`).textContent = photo.comments.length;
+  bigPicture.querySelector(`.social__caption`).textContent = photo.description;
+
+  bigPicture.querySelector(`.social__comments`).innerHTML = ``;
+  renderBigPictureComments(photo.comments, bigPicture.querySelector(`.social__comments`));
+
+  const socialCommentCount = document.querySelector(`.social__comment-count`);
+  socialCommentCount.classList.add(`hidden`);
+
+  const commentsLoader = document.querySelector(`.comments-loader`);
+  commentsLoader.classList.add(`hidden`);
+
+  const body = document.querySelector(`body`);
+  body.classList.add(`modal-open`);
+};
+
+const renderBigPictureComments = (comments, ShowMore) => {
+  for (let i = 0; i < comments.length; i++) {
+    const commentListItem = document.createElement(`li`);
+    commentListItem.classList.add(`social__comment`);
+
+    const commentImage = document.createElement(`img`);
+    commentImage.setAttribute(`src`, `${comments[i].avatar}`);
+    commentImage.setAttribute(`alt`, `${comments[i].name}`);
+    commentImage.setAttribute(`width`, `35`);
+    commentImage.setAttribute(`height`, `35`);
+    commentImage.classList.add(`social__picture`);
+
+    const commentText = document.createElement(`p`);
+    commentText.classList.add(`social__text`);
+    commentText.textContent = comments[i].message;
+
+    commentListItem.appendChild(commentImage);
+    commentListItem.appendChild(commentText);
+
+    ShowMore.appendChild(commentListItem);
+  }
+};
+
+const mockPhotos = createMockObjects(PHOTOS_AMOUNT);
+renderBigPicture(mockPhotos[3]);
