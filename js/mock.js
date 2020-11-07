b0bdd0dc1b734c3554d3af0827dd00254e36db7b
j3n4r3v <jene4ka4reva@gmail.com>
2020-11-07 12:53:53 +0200

    errors with open bigpicture

--- js/mock.js
+++ js/mock.js
@@ -3,18 +3,18 @@
   const PICTURE_TEMPLATE = document.querySelector(`#picture`)
     .content
     .querySelector(`.picture`);
-  const guide = function uuidv4() {
+  /* const guide = function uuidv4() {
     return `xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx`.replace(/[xy]/g, function (c) {
       const r = Math.random() * 16 | 0; const v = c === `x` ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
-  };
+  };  Непонял как использовать гуид, пока отключаю*/
 
   const massivePhotos = [];
 
   let renderPhoto = function (photo) {
     const pictureElement = PICTURE_TEMPLATE.cloneNode(true);
-    pictureElement.setAttribute(`id`, photo.id);
+    // pictureElement.setAttribute(`id`, photo.id); непонял как тут можно использовать айди при получении данных
     pictureElement.querySelector(`.picture__likes`).textContent = photo.likes;
     pictureElement.querySelector(`.picture__comments`).textContent = photo.comments.length;
     pictureElement.querySelector(`.picture__img`).src = `${photo.url}`;
@@ -25,7 +25,7 @@
   const createMockObjects = function (amount) {
     for (let i = 0; i < amount; i++) {
       massivePhotos.push({
-        id: guide(),
+        // id: guide(), непонял как тут можно использовать гуид при получении данных
         url: `photos/${i + 1}.jpg`,
         description: `Описание фотографии`,
         likes: Math.round(window.utils.getRandom(window.data.MIN_LIKES, window.data.MAX_LIKES)),
--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,12 @@
 'use strict';
 (function () {
   const photosFragment = document.createDocumentFragment();
-  const massivePhotos = window.mock.createMockObjects(window.data.PHOTOS_AMOUNT);
-  for (let i = 0; i < massivePhotos.length; i++) {
-    photosFragment.append(window.mock.renderPhoto(massivePhotos[i]));
-  }
-  window.data.PICTURE_CONTAINER.append(photosFragment);
-
   const bigPicture = document.querySelector(`.big-picture`);
   const closeBigPicture = bigPicture.querySelector(`.big-picture__cancel`);
   const socialCommentText = bigPicture.querySelector(`.social__footer-text`);
   const socialCommentCount = document.querySelector(`.social__comment-count`);
   const commentLoader = bigPicture.querySelector(`.comments-loader`);
+  // const massivePhotos = window.mock.massivePhotos;
 
   socialCommentCount.classList.add(`hidden`);
   commentLoader.classList.add(`hidden`);
@@ -23,10 +18,19 @@
     }
   };
 
+  const pictures = document.querySelector(`.pictures`);
+  const indexPhoto = (picture) => {
+    const photoList = pictures.querySelectorAll(`.picture`);
+    return Array.from(photoList).indexOf(picture);
+  };
+
+
   const modalOpenHandler = (evt) => {
-    for (let i = 0; i < massivePhotos.length; i++) {
-      if ((evt.target.closest(`.picture`).id) === massivePhotos[i].id) {
-        window.bigPicture.renderBigPicture(massivePhotos[i]);
+    for (let i = 0; i < window.data.PHOTOS_AMOUNT; i++) {
+      const ChoosenPhoto = evt.target.closest(`.picture`);
+      if (ChoosenPhoto) {
+        const photoId = indexPhoto(ChoosenPhoto);
+        window.bigPicture.renderBigPicture(photoId);
         bigPicture.classList.remove(`hidden`);
         document.addEventListener(`keydown`, onBigPictureEscPush);
       }
@@ -50,5 +54,27 @@
   });
 
   closeBigPicture.addEventListener(`click`, closeModalOpen);
+
+  // Добавил поведение в случае успеха/ошибки
+
+  const successHandler = function (array) {
+    for (let i = 0; i < array.length; i++) {
+      photosFragment.append(window.mock.renderPhoto(array[i]));
+    }
+    window.data.PICTURE_CONTAINER.append(photosFragment);
+  };
+
+  const errorHandler = function (errorMessage) {
+    const node = document.createElement(`div`);
+    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
+    node.style.position = `absolute`;
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = `30px`;
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  };
+
+  window.load(successHandler, errorHandler);
 
 })();
