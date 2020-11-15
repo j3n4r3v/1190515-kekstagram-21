d0b8ab895ca51b256a502faa7feb02641b99ac4d
j3n4r3v <jene4ka4reva@gmail.com>
2020-11-15 21:35:04 +0200

    add main js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,53 @@
 'use strict';
 (function () {
+  const modalOpenHandler = window.modal.modalOpenHandler;
+
+  const imgFilters = document.querySelector(`.img-filters`);
+
+  const guid = () => {
+    return `xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx`.replace(/[xy]/g, function (c) {
+      const r = Math.random() * 16 | 0; const v = c === `x` ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  };
+
+  const addListeners = () => {
+    document.querySelectorAll(`.picture`).forEach((elm) => {
+      elm.addEventListener(`click`, modalOpenHandler);
+    });
+    imgFilters.classList.remove(`img-filters--inactive`);
+  };
+
+  const successHandler = function (response) {
+    const picturesList = response.map((photo) => {
+      const obj = {id: guid()};
+      Object.assign(photo, obj);
+      return photo;
+    });
+
+    window.gallery.addServerPictures(picturesList);
+    addListeners();
+
+    window.main = {
+      picturesList
+    };
+  };
+
+  const errorHandler = function (errorMessage) {
+    const node = document.createElement(`div`);
+    node.style = `z-index: 100; margin: auto; text-align: center; background-color: green;`;
+    node.style.position = `absolute`;
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = `50px`;
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  };
+
+  window.server.load(successHandler, errorHandler);
+
+  window.main = {
+    addListeners
+  };
 
 })();
--- js/modal.js
+++ js/modal.js
@@ -4,10 +4,8 @@
   const closeBigPicture = bigPicture.querySelector(`.big-picture__cancel`);
   const socialCommentText = bigPicture.querySelector(`.social__footer-text`);
 
-  const imgFilters = document.querySelector(`.img-filters`);
   const uploadCancel = window.overlay.uploadCancel;
   const closeOverlay = window.overlay.closeOverlay;
-
 
   const onBigPictureEscPush = function (evt) {
     if (evt.key === window.utils.KEYDOWN.esc) {
@@ -18,7 +16,7 @@
 
   const modalOpenHandler = (evt) => {
     const targetId = evt.target.closest(`.picture`).getAttribute(`id`);
-    const targetObject = window.modal.picturesList.find((item) =>
+    const targetObject = window.main.picturesList.find((item) =>
       item.id === targetId);
     window.bigPicture.renderBigPicture(targetObject);
     bigPicture.classList.remove(`hidden`);
@@ -39,53 +37,8 @@
     }
   });
 
-  const guid = () => {
-    return `xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx`.replace(/[xy]/g, function (c) {
-      const r = Math.random() * 16 | 0; const v = c === `x` ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  };
-
-
-
-  const addListeners = () => {
-    document.querySelectorAll(`.picture`).forEach((elm) => {
-      elm.addEventListener(`click`, modalOpenHandler);
-    });
-    imgFilters.classList.remove(`img-filters--inactive`);
-  };
-
-  const successHandler = function (response) {
-    const picturesList = response.map((photo) => {
-      const obj = {id: guid()};
-      Object.assign(photo, obj);
-      return photo;
-    });
-
-    window.gallery.addServerPictures(picturesList);
-    addListeners();
-
-    window.modal = {
-      picturesList
-    };
-  };
-
-  const errorHandler = function (errorMessage) {
-    const node = document.createElement(`div`);
-    node.style = `z-index: 100; margin: auto; text-align: center; background-color: green;`;
-    node.style.position = `absolute`;
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = `50px`;
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement(`afterbegin`, node);
-  };
-
-  window.server.load(successHandler, errorHandler);
-
   window.modal = {
     modalOpenHandler,
-    addListeners
   };
 
 })();
